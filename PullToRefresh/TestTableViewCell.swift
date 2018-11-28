468552c5221cf6b4a17b06cc5443e6e875b4d5a6
AnupSukumaran <32585771+AnupSukumaran@users.noreply.github.com>
2018-11-28 16:49:53 +0530

    changes

---
+++ PullToRefresh/TestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.swift
+//  PullToRefresh
+//
+//  Created by Abraham VG on 28/11/18.
+//  Copyright © 2018 Wis. All rights reserved.
+//
+
+import UIKit
+
+class TestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PullToRefresh/ViewController.swift
+++ PullToRefresh/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableData: UITableView!
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = .green
+        refreshControl.addTarget(self, action: #selector(refreshAction), for: .valueChanged)
+        return refreshControl
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableData.refreshControl = refreshControl
+    }
+    
+    //MARK: call the Func which needs to referesh
+   @objc func refreshAction() {
+        print("data😄")
+        refreshControl.endRefreshing()
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestTableViewCell", for: indexPath) as! TestTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return (self.view.bounds.height - 20)
+    }
+    
+    
 }
 
